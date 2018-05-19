13be91c691a883c85ba48d5db0498b469c0671e4
talzar13 <talzar13@gmail.com>
2018-05-18 22:15:24 -0700

    Initial Commit

---
+++ Chap18 - Making Your Own Classes/Chap18 - Making Your Own Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace Chap18___Making_Your_Own_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Read();
+        }
+    }
+}
